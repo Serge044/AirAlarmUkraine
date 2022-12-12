83f9c2002abb7ffa90c626bcb2b3950cc9a0d2f8
Serge <sergiymakogon1995@gmail.com>
2022-12-12 12:49:26 +0200

    fixed function

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const kyiv = document.getElementById("KyivRegion");
+
 function consoleLogHelloKyiv() {
   console.log("Hello, it's Kyiv region ;)");
 }
+
+kyiv.addEventListener("click", consoleLogHelloKyiv);
